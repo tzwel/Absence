4596b01a90e0124c962b5052c7fa0e377bd94f05
tzwel <39600182+tzwel@users.noreply.github.com>
2021-03-27 03:23:02 +0100

    add temporary drag script

---
+++ src/js/drag.js
@@ -0,0 +1,60 @@
+const dragcontainer = document.querySelector("display-wrapper");
+const draggableImage = document.querySelector("display-wrapper img");
+
+var dragActive = false;
+var currentX;
+var currentY;
+var initialX;
+var initialY;
+var xOffset = 0;
+var yOffset = 0;
+
+dragcontainer.addEventListener("mousedown", dragStart, false);
+dragcontainer.addEventListener("mouseup", dragEnd, false);
+dragcontainer.addEventListener("mousemove", drag, false);
+
+
+function dragStart(e) {
+  if (e.type === "touchstart") {
+    initialX = e.touches[0].clientX - xOffset;
+    initialY = e.touches[0].clientY - yOffset;
+  } else {
+    initialX = e.clientX - xOffset;
+    initialY = e.clientY - yOffset;
+  }
+
+  if (e.target === draggableImage) {
+    dragActive = true;
+  }
+}
+
+function dragEnd(e) {
+  initialX = currentX;
+  initialY = currentY;
+
+  dragActive = false;
+}
+
+function drag(e) {
+  if (dragActive) {
+
+    e.preventDefault();
+
+    if (e.type === "touchmove") {
+      currentX = e.touches[0].clientX - initialX;
+      currentY = e.touches[0].clientY - initialY;
+    } else {
+      currentX = e.clientX - initialX;
+      currentY = e.clientY - initialY;
+    }
+
+    xOffset = currentX;
+    yOffset = currentY;
+
+    setTranslate(currentX, currentY, draggableImage);
+  }
+}
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+}
