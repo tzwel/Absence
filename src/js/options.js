b00fd6f3e9d4fe3caf27d886cb87f3cf11162d00
tzwel <39600182+tzwel@users.noreply.github.com>
2021-04-07 14:49:13 +0200

    add layout mirror option

--- src/js/options.js
+++ src/js/options.js
@@ -1,11 +1,21 @@
 const reflowToggle = document.querySelector(".reflowToggle");
+const mirrorLayoutToggle = document.querySelector(".mirrorLayout");
 
 reflowToggle.addEventListener("click", toggleReflow)
+mirrorLayoutToggle.addEventListener("click", toggleMirror)
 
 function toggleReflow() {
   if (reflowToggle.checked) {
     document.querySelector("grid").setAttribute("class", "reflow")
   } else {
     document.querySelector("grid").setAttribute("class", "")
+  }
+}
+
+function toggleMirror() {
+  if (mirrorLayoutToggle.checked) {
+    document.querySelector("container").setAttribute("class", "mirror")
+  } else {
+    document.querySelector("container").setAttribute("class", "")
   }
 }
