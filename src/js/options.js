95c759463e7168bb1b6ea467e74d5b0c43fa5a83
tzwel <39600182+tzwel@users.noreply.github.com>
2021-04-07 19:41:10 +0200

    add option preservation

--- src/js/options.js
+++ src/js/options.js
@@ -1,3 +1,5 @@
+
+
 const reflowToggle = document.querySelector(".reflowToggle");
 const mirrorLayoutToggle = document.querySelector(".mirrorLayout");
 
@@ -7,15 +9,32 @@ mirrorLayoutToggle.addEventListener("click", toggleMirror)
 function toggleReflow() {
   if (reflowToggle.checked) {
     document.querySelector("grid").setAttribute("class", "reflow")
+    localStorage.setItem('grid', 'reflow');
   } else {
     document.querySelector("grid").setAttribute("class", "")
+    localStorage.setItem('grid', 'normal');
   }
 }
 
 function toggleMirror() {
   if (mirrorLayoutToggle.checked) {
     document.querySelector("container").setAttribute("class", "mirror")
+    localStorage.setItem('layout', 'mirror');
   } else {
     document.querySelector("container").setAttribute("class", "")
+    localStorage.setItem('layout', 'normal');
   }
 }
+
+function readOptions() {
+  if (localStorage.layout === "mirror") {
+    document.querySelector("container").setAttribute("class", "mirror")
+    mirrorLayoutToggle.checked = true;
+  }
+  if (localStorage.grid === "reflow") {
+    reflowToggle.checked = true;
+    document.querySelector("grid").setAttribute("class", "reflow")
+  }
+}
+
+readOptions()
