869bc04f7f90c602a72bb395369c89446b8613c6
tzwel <39600182+tzwel@users.noreply.github.com>
2021-04-07 21:05:04 +0200

    add experimental favorites

---
+++ src/js/favorites.js
@@ -0,0 +1,27 @@
+let favorites = [];
+
+
+function addToFavorites() {
+  const currentImageLink = document.querySelector("display-wrapper img").src;
+  console.log(favorites);
+  console.log(localStorage.favorites);
+
+  //console.log(favorites);
+
+  if (favorites.includes(currentImageLink) || localStorage.favorites.includes(currentImageLink)) {
+    return;
+  }
+//  console.log("xd");
+  favorites.push(currentImageLink)
+  //localStorage.setItem('favorites', localStorage.favorites + currentImageLink + ", ");
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+  favorites = JSON.parse(localStorage.favorites);
+
+}
+
+function resetFavorites() {
+  localStorage.favorites = "";
+  favorites = "";
+}
+
+document.querySelector(".add-to-favorites").addEventListener("click", addToFavorites)
