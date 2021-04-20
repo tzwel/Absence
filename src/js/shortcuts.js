bc9fd986406fa873ab3f108d8f7a8c9118db1cac
tzwel <39600182+tzwel@users.noreply.github.com>
2021-04-20 11:16:55 +0200

    arrow shortcuts optimizations

--- src/js/shortcuts.js
+++ src/js/shortcuts.js
@@ -14,18 +14,17 @@ document.addEventListener("keydown", function(event) {
   }
 });
 
-// previous arrow shortcut
-document.addEventListener("keydown", function(event) {
-  if (event.keyCode === 37) {
-    event.preventDefault();
-    previousFetch()
-  }
-});
 
-// previous arrow shortcut
+// next/previous arrow shortcut
 document.addEventListener("keydown", function(event) {
-  if (event.keyCode === 39) {
-    event.preventDefault();
-    nextFetch()
+  if (!document.querySelector("display-wrapper").classList.contains("open")) {
+    if (event.keyCode === 39) {
+      event.preventDefault();
+      nextFetch()
+    }
+    if (event.keyCode === 37) {
+      event.preventDefault();
+      previousFetch()
+    }
   }
 });
