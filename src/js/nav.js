c7e6b99801d5291676fc066a21580abbdc8c0b47
tzwel <39600182+tzwel@users.noreply.github.com>
2021-04-20 11:45:19 +0200

    add fullscreen button

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,5 +1,8 @@
-document.querySelector(".minimize-button").addEventListener("click", minimize)
-document.querySelector(".close-button").addEventListener("click", close)
+document.querySelector(".minimize-button").addEventListener("click", minimize);
+document.querySelector(".fullscreen-button").addEventListener("click", fullscreen);
+document.querySelector(".close-button").addEventListener("click", close);
+
+//localStorage.setItem('fullscreen', 'off');
 
 function minimize() {
   remote.getCurrentWindow().minimize();
@@ -7,4 +10,20 @@ function minimize() {
 
 function close() {
   remote.getCurrentWindow().close();
+}
+
+// temporary solution
+if (localStorage.fullscreen === "on") {
+  remote.getCurrentWindow().maximize();
+}
+
+
+function fullscreen() {
+  if (localStorage.fullscreen === "off") {
+    localStorage.setItem('fullscreen', 'on');
+    remote.getCurrentWindow().maximize();
+  } else {
+    remote.getCurrentWindow().unmaximize();
+    localStorage.setItem('fullscreen', 'off');
+  }
 }
