508208ef2573204487b2eff8e61aec8b5ca6a71d
tzwel <39600182+tzwel@users.noreply.github.com>
2021-08-26 03:26:13 +0200

    pelno rzeczy kto by to spamietal i rozpisal

--- src/js/detailedimageview.js
+++ src/js/detailedimageview.js
@@ -3,7 +3,7 @@ function loadDetails() {
   console.log(resp[clickedNumber]);
   drawer.innerHTML = "";
   // temporary solution
-  drawer.insertAdjacentHTML("beforeend", ` <h2> Tags </h2> <span> ${resp[clickedNumber]["tags"]} </span>`);
+  drawer.insertAdjacentHTML("beforeend", ` <h2> Tags </h2> <span style="max-height: 200px; overflow-y: auto" > ${resp[clickedNumber]["tags"]} </span>`);
   drawer.insertAdjacentHTML("beforeend", `<h2> Source </h2> <span> ${resp[clickedNumber]["source"]} </span>`);
   drawer.insertAdjacentHTML("beforeend", `<h2> Rating </h2> <span> ${resp[clickedNumber]["rating"]} </span>`);
   drawer.insertAdjacentHTML("beforeend", `<h2> Likes </h2> <span> ${resp[clickedNumber]["score"]} </span>`);
