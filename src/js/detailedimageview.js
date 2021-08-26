da8aa153260a8c5f17ad2bbc012fd630e32f0bf9
tzwel <39600182+tzwel@users.noreply.github.com>
2021-08-26 04:30:02 +0200

    zajebiste tagi

--- src/js/detailedimageview.js
+++ src/js/detailedimageview.js
@@ -9,7 +9,7 @@ function loadDetails() {
  // console.log(kurwa);
 
   // temporary solution
-  drawer.insertAdjacentHTML("beforeend", ` <h2> Tags </h2> <span style="max-height: 200px; overflow-y: auto" > ${resp[clickedNumber]["tags"]} </span>`);
+  drawer.insertAdjacentHTML("beforeend", ` <h2> Tags </h2> <span style="max-height: 200px; overflow-y: auto" > ${resp[clickedNumber]["tags"].replace(/ /g, '</br>')} </span>`);
   drawer.insertAdjacentHTML("beforeend", `<h2> Source </h2> <span> ${resp[clickedNumber]["source"]} </span>`);
   drawer.insertAdjacentHTML("beforeend", `<h2> Rating </h2> <span> ${resp[clickedNumber]["rating"]} </span>`);
   drawer.insertAdjacentHTML("beforeend", `<h2> Likes </h2> <span> ${resp[clickedNumber]["score"]} </span>`);
