2ba2e8d348a9f65b8aea4dacc1a558571cdb9c2c
tzwel <39600182+tzwel@users.noreply.github.com>
2021-10-13 16:42:12 +0200

    blacklist

--- src/renderer.js
+++ src/renderer.js
@@ -6,4 +6,4 @@ const { dialog, app } = require("electron").remote;
 
 const savePath = "./downloaded_images";
 
-blacklists.personal = "-1boy -scat -pregnant -fat -gay -futabu -futanari -2boys -abs -yaoi -futanari -futa -gigantic_breasts -large_areolae -trap -furry -amputee -pubic_stubble -nolia -chouzuki_maryou -death -bestiality -newhalf -shemale -trap -no_humans -pokemon -prolapse -loli";
+blacklists.personal = "-1boy -scat -pregnant -fat -gay -futabu -futanari -2boys -abs -yaoi -futanari -futa -gigantic_breasts -large_areolae -trap -furry -amputee -pubic_stubble -nolia -chouzuki_maryou -death -bestiality -newhalf -shemale -trap -no_humans -pokemon -prolapse -loli -white_fur -wolf";
