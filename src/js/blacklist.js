99df406ef1ed33836e1aa3e2bb53ad71bc0f02d9
tzwel <39600182+tzwel@users.noreply.github.com>
2021-10-29 23:43:47 +0200

    blacklist

--- src/js/blacklist.js
+++ src/js/blacklist.js
@@ -5,5 +5,5 @@ const blacklists = {
 
     nomen: "-1boy -yaoi -futanari -boy -man -men -trap -shemale -gay -shota -3boys",
 
-    enhanced: "-yaoi -dark_skin -dark_skinned_male -dark_skinned_female -multiple_boys -old_man -asian -3d -uterine_prolapse -dog -pig -ugly_man -vore -milking_machine -horse -horse_penis -goldfish -male_focus -naruto -deer -prolapse -anal_hair"
+    enhanced: "-yaoi -dark_skin -dark_skinned_male -dark_skinned_female -multiple_boys -old_man -asian -3d -uterine_prolapse -dog -pig -ugly_man -vore -milking_machine -horse -horse_penis -goldfish -male_focus -naruto -deer -prolapse -anal_hair -uncanny_valley"
 };
